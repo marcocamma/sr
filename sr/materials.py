e2339ad452c16f8b14781e58eca9db2fc367a010
Marco Cammarata <marco.cammarata@esrf.eu>
2025-01-20 18:31:22 +0100

    added dose calculation and ruff check/format

--- sr/materials.py
+++ sr/materials.py
@@ -4,16 +4,19 @@ In this module:
     densities in gr/cm3
     transmissions is calculated using PHOTOELECTRIC effect ONLY
 """
+
 import numpy as np
-import itertools
-import collections
 import copy
 
 from functools import lru_cache
+
 try:
     from .utils.conversion import energy_to_wavelength
 except ImportError:
-    def energy_to_wavelength(energy): return 12.398/energy
+
+    def energy_to_wavelength(energy):
+        return 12.398 / energy
+
 
 import xraydb as xdb
 
@@ -140,7 +143,7 @@ AVOGADRO = 6.02214199e23
 
 def list_materials():
     print(f"{'name':15s} {'formula':25s} {'density':15s}")
-    for name,formula in CHEMICAL_FORMULAS.items():
+    for name, formula in CHEMICAL_FORMULAS.items():
         density = DENSITIES[name]
         print(f"{name:15s} {formula:25s} {density:15.3f} gr/cm3")
 
@@ -150,34 +153,39 @@ def get_default_density(element):
         raise ValueError("No default density for", element)
     return DENSITIES[element]
 
+
 def get_formula(element):
     if element in CHEMICAL_FORMULAS:
         element = CHEMICAL_FORMULAS[element]
     return element
 
-def get_material(element,density=None):
+
+def get_material(element, density=None):
     # density has to go first else names are converted into formulas
-    if density is None: density = get_default_density(element)
+    if density is None:
+        density = get_default_density(element)
     element = get_formula(element)
-    return element,density
+    return element, density
+
 
 def get_atomic_densities(element, density=None):
-    """ returns atoms in cm^3 for each atomic species (in dictionary)"""
-    molecular_density = get_number_density(element,density=density)
+    """returns atoms in cm^3 for each atomic species (in dictionary)"""
+    molecular_density = get_number_density(element, density=density)
     temp = xdb.materials.chemparse(element)
-    ret = dict( [ (el,n*molecular_density) for el,n in temp.items() ] )
+    ret = dict([(el, n * molecular_density) for el, n in temp.items()])
     return ret
 
 
 def get_molecular_mass(element):
     molecular_mass = 0
     temp = xdb.materials.chemparse(element)
-    for el,n in temp.items():
-        molecular_mass += n*xdb.atomic_mass(el)
+    for el, n in temp.items():
+        molecular_mass += n * xdb.atomic_mass(el)
     return molecular_mass
- 
+
+
 def get_number_density(element, density=None):
-    """ returns number of molecules per cm^3 """
+    """returns number of molecules per cm^3"""
     if density is None:
         if element not in DENSITIES:
             raise ValueError("No default density for", element)
@@ -187,44 +195,47 @@ def get_number_density(element, density=None):
     num_density = density / molecular_mass * AVOGADRO
     return num_density
 
+
 def get_molecules_per_cm3(element, density=None):
-    return get_number_density(element,density=density)
+    return get_number_density(element, density=density)
+
 
 def get_molar_density(element, density=None):
-    """ return molar density (= number of moles per L) """
-    n = get_number_density(element,density=density)
-    number_of_moles_per_cm3 = n/AVOGADRO
-    number_of_moles_per_L = number_of_moles_per_cm3*1e3
+    """return molar density (= number of moles per L)"""
+    n = get_number_density(element, density=density)
+    number_of_moles_per_cm3 = n / AVOGADRO
+    number_of_moles_per_L = number_of_moles_per_cm3 * 1e3
     return number_of_moles_per_L
 
+
 def get_density(element, molar_density=1e-3):
-    """ return density (in gr/cm3) """
+    """return density (in gr/cm3)"""
     mass_per_mole = get_molecular_mass(element)
-    num_moles_per_cm3 = molar_density*1e-3 # 1cm3 = 1e-3L
-    return mass_per_mole*num_moles_per_cm3
+    num_moles_per_cm3 = molar_density * 1e-3  # 1cm3 = 1e-3L
+    return mass_per_mole * num_moles_per_cm3
+
 
 def get_air_density(T=293, P=1e5):
     density_kg_m3 = P * 0.0289652 / (8.31446 * T)
-    density_g_cm3 = density_kg_m3 * 1e-3
     return density_kg_m3
 
 
 @lru_cache(maxsize=1024)
 def get_refractive_index(material, density=None, energy=10):
-    material,density = get_material(material, density)
+    material, density = get_material(material, density)
     if isinstance(energy, (float, int, np.ndarray)):
         energy = energy * 1e3
-        ret = m.get_refractive_index(energy)
+        ret = material.get_refractive_index(energy)
     else:
         # for tuples
-        ret = [m.get_refractive_index(e * 1e3) for e in energy]
+        ret = [material.get_refractive_index(e * 1e3) for e in energy]
         ret = np.asarray(ret)
     return ret
 
 
 @lru_cache(maxsize=1024)
 def get_delta_beta(material, density=None, energy=10):
-    material,density = get_material(material, density)
+    material, density = get_material(material, density)
     delta, beta, attlen = xdb.xray_delta_beta(material, density, energy * 1e3)
     return delta, beta
 
@@ -235,11 +246,11 @@ def _attenuation_length_nocache(material, density=None, energy=10, kind="total")
                 Transmission is then exp(-thickness/attenuation_length)
     kind can be "total" or "photo" (for photoelectric)
     """
-    if isinstance(energy,(tuple,list)):
-        energy = [e*1e3 for e in energy]
+    if isinstance(energy, (tuple, list)):
+        energy = [e * 1e3 for e in energy]
     else:
-        energy = energy*1e3
-    material,density = get_material(material, density)
+        energy = energy * 1e3
+    material, density = get_material(material, density)
     mu = xdb.material_mu(material, energy, density=density, kind=kind)
     mu = mu * 1e2  # (from cm-1 to m-1)
     return 1 / mu
@@ -251,39 +262,48 @@ def attenuation_length(compound, density=None, energy=None, kind="total"):
     Calculates the attenuation length for a compound [in m]
                 Transmisison is then exp(-thickness/attenuation_length)
     """
-    return _attenuation_length_nocache(compound, density=density, energy=energy, kind=kind)
+    return _attenuation_length_nocache(
+        compound, density=density, energy=energy, kind=kind
+    )
 
 
-def transmission(material="Si", thickness=1e-3, energy=10, density=None, cross_section_kind="total"):
-    """ kind can be total|photo """
+def transmission(
+    material="Si", thickness=1e-3, energy=10, density=None, cross_section_kind="total"
+):
+    """kind can be total|photo"""
     w = Wafer(material, thickness=thickness, density=density)
     return w.calc_transmission(energy, cross_section_kind=cross_section_kind)
 
-def absorption(material="Si", thickness=1e-3, energy=10, density=None, cross_section_kind="photo"):
-    """ kind can be total|photo """
-    w = Wafer(material, thickness=thickness, density=density)
-    return 1-w.calc_transmission(energy, cross_section_kind=cross_section_kind)
 
+def absorption(
+    material="Si", thickness=1e-3, energy=10, density=None, cross_section_kind="photo"
+):
+    """kind can be total|photo"""
+    w = Wafer(material, thickness=thickness, density=density)
+    return 1 - w.calc_transmission(energy, cross_section_kind=cross_section_kind)
 
 
 class Wafer:
     def __init__(self, name="Si", thickness=10e-6, density=None):
         self.name = name
-        formula,density = get_material(name, density)
-        self.formula=formula
+        formula, density = get_material(name, density)
+        self.formula = formula
         self.density = float(density)
         self.thickness = thickness
 
     def get_att_len(self, E, kind="total"):
-        """ get the attenuation length (in meter) of material
-            E in keV"""
+        """get the attenuation length (in meter) of material
+        E in keV"""
         try:
             return attenuation_length(
                 self.formula, density=self.density, energy=E, kind=kind
             )
         except TypeError:  # when E is unhashable (ndarray)
             return attenuation_length(
-                self.formula, density=self.density, energy=tuple(E), kind=kind,
+                self.formula,
+                density=self.density,
+                energy=tuple(E),
+                kind=kind,
             )
 
     def calc_transmission(self, E, cross_section_kind="total"):
@@ -295,8 +315,9 @@ class Wafer:
         return 1 - np.exp(-self.thickness / att_len)
 
     def photon_flux_after_wafer(self, photon_flux_before, cross_section_kind="total"):
-        """ does not take into account finite size of mirrors """
+        """does not take into account finite size of mirrors"""
         from . import undulator
+
         photon_flux_before = copy.deepcopy(photon_flux_before)
         t = self.calc_transmission(
             photon_flux_before.energy, cross_section_kind=cross_section_kind
@@ -306,7 +327,7 @@ class Wafer:
         after = undulator._photon_flux_density_helper(photon_flux_before)
         return after
 
-    def material_str(self,with_formula=False,with_density=False,with_thickness=True):
+    def material_str(self, with_formula=False, with_density=False, with_thickness=True):
         s = self.name
         if with_formula and self.name != self.formula:
             s += f"[{self.formula}]"
@@ -319,11 +340,28 @@ class Wafer:
                 s += f", {self.thickness*1e6:.2f}μm"
         return s
 
+    def dose(self, E, f=1e12, exp_time=1, beamsize=50e-6, verbose=False):
+        """returns absorbed dose in Gray (Gy), f is the photon flux (photons/s)"""
+        photon_energy = 1.60218e-19 * E * 1e3
+        beam_power = photon_energy * f
+        A = self.calc_absorption(E)
+        absorbed_energy = beam_power * exp_time * A
+        density_SI = self.density * 1e3
+        mass = beamsize * beamsize * self.thickness * density_SI
+        if verbose:
+            print(f"{beam_power=:.3e} W ,{absorbed_energy=:-4e} J ,{mass=:.4e} Kg")
+        return absorbed_energy / mass
+
     def __repr__(self):
-        return self.material_str(with_formula=True,with_density=True,with_thickness=True)
+        return self.material_str(
+            with_formula=True, with_density=True, with_thickness=True
+        )
 
     def __str__(self):
-        return self.material_str(with_formula=False,with_density=False,with_thickness=True)
+        return self.material_str(
+            with_formula=False, with_density=False, with_thickness=True
+        )
+
 
 def examples():
     Si = Wafer("Si", thickness=10e-6)
